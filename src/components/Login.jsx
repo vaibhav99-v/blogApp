75f2c2c3774cbb2b07ccd9bc2392b10d4dcf9cb1
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2025-01-18 11:16:12 +0530

    Update Login2.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,16 +28,16 @@ function Login() {
 
   return (
     <div
-    className='flex items-center justify-center w-full'
+    className='flex items-center justify-center w-full m-3'
     >
-        <div className={`mx-auto w-full max-w-lg bg-gray-800 text-white rounded-xl p-10 border border-black/10`}>
+        <div className={`mx-auto w-full max-w-lg bg-gray-700 rounded-xl p-10 border border-white/10`}>
         <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-        <p className="mt-2 text-center text-base text-black/60">
+        <p className="mt-2 text-center text-base text-white/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
                         to="/signup"
